3d2d7e08236945e6bba3a9be3699d0ff22403383
acoulton <andrew@ingenerator.com>
2020-11-09 13:14:00 +0000

    Expose the remaining Cloud Tasks headers on the TaskRequest object

--- src/Server/TaskRequest.php
+++ src/Server/TaskRequest.php
@@ -6,6 +6,12 @@ namespace Ingenerator\CloudTasksWrapper\Server;
 
 use Psr\Http\Message\ServerRequestInterface;
 
+/**
+ * Encapsulates common details of the incoming task request from the underlying HTTP request
+ *
+ * Exposes the header properties per https://cloud.google.com/tasks/docs/creating-http-target-tasks
+ * as well as application-specific metadata.
+ */
 class TaskRequest
 {
     protected ServerRequestInterface $request;
@@ -17,6 +23,11 @@ class TaskRequest
         $this->task_type = $task_type;
     }
 
+    /**
+     * The complete external URL that was called to trigger this task handler
+     *
+     * @return string
+     */
     public function getFullUrl(): string
     {
         return $this->request->getUri();
@@ -25,6 +36,113 @@ class TaskRequest
     public function getTaskType(): string
     {
         return $this->task_type;
+    }
+
+    /**
+     * The name of the queue
+     *
+     * @return string
+     * @see https://cloud.google.com/tasks/docs/creating-http-target-tasks#handler
+     */
+    public function getQueueName(): string
+    {
+        return $this->request->getHeaderLine('X-CloudTasks-QueueName');
+    }
+
+    /**
+     * The "short" name of the task, or, if no name was specified at creation, a unique system-generated id.
+     *
+     * This is the my-task-id value in the complete task name, ie, task_name =
+     * projects/my-project-id/locations/my-location/queues/my-queue-id/tasks/my-task-id.
+     *
+     * @return string
+     * @see https://cloud.google.com/tasks/docs/creating-http-target-tasks#handler
+     */
+    public function getTaskName(): string
+    {
+        return $this->request->getHeaderLine('X-CloudTasks-TaskName');
+    }
+
+    /**
+     * The number of times this task has been retried. For the first attempt, this value is 0.
+     *
+     * This number includes attempts where the task failed due to 5XX error codes and never reached
+     * the execution phase.
+     *
+     * @return int
+     * @see https://cloud.google.com/tasks/docs/creating-http-target-tasks#handler
+     */
+    public function getRetryCount(): int
+    {
+        return (int) $this->request->getHeaderLine('X-CloudTasks-TaskRetryCount');
+    }
+
+    /**
+     * The total number of times that the task has received a response from the handler.
+     *
+     * Since Cloud Tasks deletes the task once a successful response has been received, all previous
+     * handler responses were failures. This number does not include failures due to 5XX error
+     * codes.
+     *
+     * @return int
+     * @see https://cloud.google.com/tasks/docs/creating-http-target-tasks#handler
+     */
+    public function getExecutionCount(): int
+    {
+        return (int) $this->request->getHeaderLine('X-CloudTasks-TaskExecutionCount');
+    }
+
+    /**
+     * The HTTP response code from the previous retry.
+     *
+     * @return string|null
+     * @see https://cloud.google.com/tasks/docs/creating-http-target-tasks#handler
+     */
+    public function getPreviousResponse(): ?string
+    {
+        return $this->request->getHeaderLine('X-CloudTasks-TaskPreviousResponse') ?: NULL;
+    }
+
+    /**
+     * The reason for retrying the task.
+     *
+     * @return string|null
+     * @see https://cloud.google.com/tasks/docs/creating-http-target-tasks#handler
+     */
+    public function getRetryReason(): ?string
+    {
+        return $this->request->getHeaderLine('X-CloudTasks-TaskRetryReason') ?: NULL;
+    }
+
+    /**
+     * The schedule time of the task
+     *
+     * @return \DateTimeImmutable|null
+     * @see https://cloud.google.com/tasks/docs/creating-http-target-tasks#handler
+     */
+    public function getScheduledTime(): ?\DateTimeImmutable
+    {
+        $time = $this->request->getHeaderLine('X-CloudTasks-TaskETA');
+        if (empty($time)) {
+            return NULL;
+        }
+
+        // @todo: throw if unexpected / invalid format?
+        // @todo: this needs to go to php-utils
+        // Work round some PHP edge cases.
+        // First, a float with a .00 casts to string with no decimal point, which causes the createFromFormat to fail
+        // on exact seconds. Use sprintf to ensure there's always a decimal present
+        $dt = \DateTimeImmutable::createFromFormat('U.u', sprintf('%.6F', (float) $time));
+
+        // Second, creating from unix timestamp of any kind always sets the TZ to UTC (*even* if you specify a timezone
+        // in the constructor).
+        // So cast it back to the default timezone for consistency with other date time constructor formats
+        return $dt->setTimezone(new \DateTimeZone(\date_default_timezone_get()));
+    }
+
+    public function getHttpRequest(): ServerRequestInterface
+    {
+        return $this->request;
     }
 
 }
--- test/unit/Server/TaskRequestTest.php
+++ test/unit/Server/TaskRequestTest.php
@@ -6,6 +6,7 @@ namespace test\unit\Ingenerator\CloudTasksWrapper\Server;
 
 use GuzzleHttp\Psr7\ServerRequest;
 use Ingenerator\CloudTasksWrapper\Server\TaskRequest;
+use Ingenerator\PHPUtils\DateTime\DateString;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ServerRequestInterface;
 
@@ -26,6 +27,102 @@ class TaskRequestTest extends TestCase
             'https://my.app/task?foo=bar',
             $this->newSubject()->getFullUrl()
         );
+    }
+
+    /**
+     * @testWith [{"X-CloudTasks-TaskETA":"1501912403.555197"}, "2017-08-05 06:53:23.555197 +01:00"]
+     *           [{}, null]
+     */
+    public function test_it_provides_cloud_tasks_eta_as_datetime_or_null($hdrs, $expect)
+    {
+        $dtz = \date_default_timezone_get();
+        try {
+            \date_default_timezone_set('Europe/London');
+
+            $this->http_req = new ServerRequest('POST', 'https://my.app/task?foo=bar', $hdrs);
+            $this->assertSame(
+                $expect,
+                DateString::format($this->newSubject()->getScheduledTime(), 'Y-m-d H:i:s.u P', NULL)
+            );
+        } finally {
+            \date_default_timezone_set($dtz);
+        }
+    }
+
+    public function test_it_provides_queue_name()
+    {
+        $this->http_req = new ServerRequest(
+            'POST',
+            'https://my.app/task?foo=bar',
+            [
+                'X-CloudTasks-QueueName' => 'my-queue'
+            ]
+        );
+        $this->assertSame('my-queue', $this->newSubject()->getQueueName());
+    }
+
+    public function test_it_provides_task_name()
+    {
+        $this->http_req = new ServerRequest(
+            'POST',
+            'https://my.app/task?foo=bar',
+            [
+                'X-CloudTasks-TaskName' => 'task12472315223'
+            ]
+        );
+        $this->assertSame('task12472315223', $this->newSubject()->getTaskName());
+    }
+
+    public function test_it_provides_retry_count()
+    {
+        $this->http_req = new ServerRequest(
+            'POST',
+            'https://my.app/task?foo=bar',
+            [
+                'X-CloudTasks-TaskRetryCount' => '5'
+            ]
+        );
+        $this->assertSame(5, $this->newSubject()->getRetryCount());
+    }
+
+    public function test_it_provides_execution_count()
+    {
+        $this->http_req = new ServerRequest(
+            'POST',
+            'https://my.app/task?foo=bar',
+            [
+                'X-CloudTasks-TaskExecutionCount' => '3'
+            ]
+        );
+        $this->assertSame(3, $this->newSubject()->getExecutionCount());
+    }
+
+    /**
+     * @testWith [{"X-CloudTasks-TaskPreviousResponse": "499"}, "499"]
+     *           [{}, null]
+     */
+    public function test_it_provides_previous_response_if_present($hdrs, $expect)
+    {
+        $this->http_req = new ServerRequest(
+            'POST',
+            'https://my.app/task?foo=bar',
+            $hdrs
+        );
+        $this->assertSame($expect, $this->newSubject()->getPreviousResponse());
+    }
+
+    /**
+     * @testWith [{"X-CloudTasks-TaskRetryReason": "didn't work before"}, "didn't work before"]
+     *           [{}, null]
+     */
+    public function test_it_provides_retry_reason_if_present($hdrs, $expect)
+    {
+        $this->http_req = new ServerRequest(
+            'POST',
+            'https://my.app/task?foo=bar',
+            $hdrs
+        );
+        $this->assertSame($expect, $this->newSubject()->getRetryReason());
     }
 
     protected function setUp(): void
